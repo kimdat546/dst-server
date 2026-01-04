14c281a7bbd8d6fdb084d76f57d15037223b7ee6
kimdat546 <kimdat546@gmail.com>
2026-01-04 10:51:32 +0700

    feat: Update DST server image, expand server configuration with new environment variables, and integrate mod setup files.

---
+++ config/dedicated_server_mods_setup.lua
@@ -0,0 +1,3 @@
+ServerModSetup("375859599")
+ServerModSetup("378160973")
+ServerModSetup("1185229307")
---
+++ config/modoverrides.lua
@@ -0,0 +1,5 @@
+return {
+  ["workshop-375859599"] = { enabled = true },
+  ["workshop-378160973"] = { enabled = true },
+  ["workshop-1185229307"] = { enabled = true }
+}
