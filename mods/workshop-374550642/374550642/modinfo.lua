734eebb0611db0fc3a1256f749595a053055d51a
kimdat546 <kimdat546@gmail.com>
2026-02-24 22:54:01 +0700

    feat: Introduce a new mod enabling configurable item stack sizes for various item categories.

---
+++ mods/workshop-374550642/374550642/modinfo.lua
@@ -0,0 +1,181 @@
+--The name of the mod displayed in the 'mods' screen.
+name = "Increased Stack size"
+
+--A description of the mod.
+description = "Changes the maximum size of itemstacks of all stackable items to the configured amount."
+
+author = "ChaosMind42"
+ 
+version = "2.2"
+
+api_version = 10
+
+-- Compatible with both the base game, reign of giants, and dst
+dont_starve_compatible = true
+reign_of_giants_compatible = true
+dst_compatible = true
+
+forumthread = ""
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+
+--These let clients know if they need to get the mod from the Steam Workshop to join the game
+all_clients_require_mod = true
+clients_only_mod = false
+
+--This lets people search for servers with this mod by these tags
+server_filter_tags = {"utility"}
+
+-- ModConfiguration option
+configuration_options =
+{
+	{
+		name = "STACK_SIZE_LARGEITEM",
+		label = "Max stacksize for large items",
+		options =	{
+						{description = "5", data = 5},
+						{description = "10 (Default)", data = 10},
+						{description = "20", data = 20},
+						{description = "40", data = 40},
+						{description = "60", data = 60},
+						{description = "80", data = 80},
+						{description = "99", data = 99},
+						{description = "120", data = 120},
+						{description = "150", data = 150},
+						{description = "200", data = 200},
+						{description = "250", data = 250},
+						{description = "300", data = 300},
+						{description = "400", data = 400},
+						{description = "500", data = 500},
+						{description = "600", data = 600},
+						{description = "700", data = 700},
+						{description = "800", data = 800},
+						{description = "900", data = 900},
+						{description = "999", data = 999},
+					},
+
+		default = 99,
+	},
+	{
+		name = "STACK_SIZE_MEDITEM",
+		label = "Max stacksize for medium items",
+		options =	{
+						{description = "5", data = 5},
+						{description = "10", data = 10},
+						{description = "20 (Default)", data = 20},
+						{description = "40", data = 40},
+						{description = "60", data = 60},
+						{description = "80", data = 80},
+						{description = "99", data = 99},
+						{description = "120", data = 120},
+						{description = "150", data = 150},
+						{description = "200", data = 200},
+						{description = "250", data = 250},
+						{description = "300", data = 300},
+						{description = "400", data = 400},
+						{description = "500", data = 500},
+						{description = "600", data = 600},
+						{description = "700", data = 700},
+						{description = "800", data = 800},
+						{description = "900", data = 900},
+						{description = "999", data = 999},
+					},
+
+		default = 99,
+	},
+	{
+		name = "STACK_SIZE_SMALLITEM",
+		label = "Max stacksize for small items",
+		options =	{
+						{description = "5", data = 5},
+						{description = "10", data = 10},
+						{description = "20", data = 20},
+						{description = "40 (Default)", data = 40},
+						{description = "60", data = 60},
+						{description = "80", data = 80},
+						{description = "99", data = 99},
+						{description = "120", data = 120},
+						{description = "150", data = 150},
+						{description = "200", data = 200},
+						{description = "250", data = 250},
+						{description = "300", data = 300},
+						{description = "400", data = 400},
+						{description = "500", data = 500},
+						{description = "600", data = 600},
+						{description = "700", data = 700},
+						{description = "800", data = 800},
+						{description = "900", data = 900},
+						{description = "999", data = 999},
+					},
+
+		default = 99,
+	},
+	{
+		name = "STACK_SIZE_TINYITEM",
+		label = "Max stacksize for tiny items",
+		options =	{
+						{description = "5", data = 5},
+						{description = "10", data = 10},
+						{description = "20", data = 20},
+						{description = "40", data = 40},
+						{description = "60 (Default)", data = 60},
+						{description = "80", data = 80},
+						{description = "99", data = 99},
+						{description = "120", data = 120},
+						{description = "150", data = 150},
+						{description = "200", data = 200},
+						{description = "250", data = 250},
+						{description = "300", data = 300},
+						{description = "400", data = 400},
+						{description = "500", data = 500},
+						{description = "600", data = 600},
+						{description = "700", data = 700},
+						{description = "800", data = 800},
+						{description = "900", data = 900},
+						{description = "999", data = 999},
+					},
+
+		default = 99,
+	},
+	{
+		name = "STACK_SIZE_PELLET",
+		label = "Max stacksize for pellet items",
+		options =	{
+						{description = "5", data = 5},
+						{description = "10", data = 10},
+						{description = "20", data = 20},
+						{description = "40", data = 40},
+						{description = "60", data = 60},
+						{description = "80", data = 80},
+						{description = "99", data = 99},
+						{description = "120 (Default)", data = 120},
+						{description = "150", data = 150},
+						{description = "200", data = 200},
+						{description = "250", data = 250},
+						{description = "300", data = 300},
+						{description = "400", data = 400},
+						{description = "500", data = 500},
+						{description = "600", data = 600},
+						{description = "700", data = 700},
+						{description = "800", data = 800},
+						{description = "900", data = 900},
+						{description = "999", data = 999},
+					},
+
+		default = 120,
+	},
+	{
+		name = "FORCE_STACKSIZES",
+		label = "Force overwrite stacksizes",
+		hover = "Experimental feature to make this mod more compatible(or less) with other mods depending on mod load order, use with caution",
+		options =	{
+						{description = "Deactivated", data = false},
+						{description = "Activated", data = true},
+					},
+
+		default = false,
+	},
+}
+
+priority = 0.00374550642
